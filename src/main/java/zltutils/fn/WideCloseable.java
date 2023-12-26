f049f56f79f7b0b69a858941fbe776a511f8f942
apuiji <2330683684@qq.com>
2023-12-26 16:12:12 +0800

    a

--- src/main/java/zltutils/fn/WideCloseable.java
+++ src/main/java/zltutils/fn/WideCloseable.java
@@ -12,8 +12,16 @@ public interface WideCloseable {
 		}
 	}
 
+	static void close(Throwable[] outThrown, AutoCloseable c, int i) {
+		close(outThrown, c::close);
+	}
+
 	static NarrowCloseable narrow(Throwable[] outThrown, WideCloseable c) {
 		return () -> close(outThrown, c);
+	}
+
+	static NarrowCloseable narrow(Throwable[] outThrown, AutoCloseable c, int i) {
+		return () -> close(outThrown, c, 0);
 	}
 
 	void close() throws Throwable;
