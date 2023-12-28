4158075b4362ae6d042f6447cf967ab5a2e8527b
apuiji <2330683684@qq.com>
2023-12-28 16:10:52 +0800

    a

--- src/main/java/zltutils/refl/ReflectMemberUtils.java
+++
@@ -1,9 +0,0 @@
-package zltutils.refl;
-
-import java.lang.reflect.Member;
-
-public class ReflectMemberUtils {
-	public static boolean isModified(Member mem, int mod) {
-		return (mem.getModifiers() & mod) != mod;
-	}
-}
